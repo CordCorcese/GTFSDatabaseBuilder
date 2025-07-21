e10c2e8a33d2247989ecb538231781702b09b833
Cord Corcese <badwithnames14@gmail.com>
2025-07-21 01:28:13 -0700

    creates BC Transit database from GTFS files successfully

--- DatabaseCreator.py
+++ DatabaseCreator.py
@@ -5,30 +5,60 @@ cursor = database.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS Stop (stopId Int PRIMARY KEY,stopName String,stopLat Double,stopLon Double,wheelchair Int,stopCode Int)") 
 cursor.execute("CREATE TABLE IF NOT EXISTS Route (routeID String PRIMARY KEY,routeShortName String,routeLongName String,routeType Int,routeColour String,routeTextColour String)") 
 cursor.execute("CREATE TABLE IF NOT EXISTS Trip (routeID String,serviceID Int,tripID String PRIMARY KEY,tripHeadSign String,shapeID Int,blockID Int,directionID Int)") 
-cursor.execute("CREATE TABLE IF NOT EXISTS StopTime (tripID String,arrivalTime String,departureTime String,stopID Int,stopSequence Int,shapeDistTravelled Int,stopHeadSign Int,pickupType Int,dropOffType Int,PRIMARY KEY (tripID, stopSequence))") 
-cursor.execute("CREATE TABLE IF NOT EXISTS CalendarDate (serviceID Int,date String PRIMARY KEY,exceptionType Int)") 
-cursor.execute("CREATE TABLE IF NOT EXISTS Shape (shapeID Int,shapePtLat Double,shapePtLon Double,tripHeadSign String,shapePtSequence Int,shapeDistTravelled Int,PRIMARY KEY (shapeID, shapePtSequence))") 
+cursor.execute("CREATE TABLE IF NOT EXISTS StopTime (tripID String,arrivalTime String,departureTime String,stopID Int,stopSequence Int,shapeDistTravelled Int,stopHeadSign Int,pickupType Int,dropOffType Int,timePoint INT, PRIMARY KEY (tripID, stopSequence))") 
+cursor.execute("CREATE TABLE IF NOT EXISTS CalendarDate (serviceID Int,date String,exceptionType Int, PRIMARY KEY(serviceID, date))") 
+cursor.execute("CREATE TABLE IF NOT EXISTS Shape (shapeID Int,shapePtLat Double,shapePtLon Double,shapePtSequence Int,shapeDistTravelled Int,PRIMARY KEY (shapeID, shapePtSequence))") 
 
+stopTimeInsertSQL = '''INSERT INTO StopTime (tripID, arrivalTime, departureTime, stopID, stopSequence, shapeDistTravelled, stopHeadSign, pickupType, dropOffType, timepoint) VALUES (?,?,?,?,?,?,?,?,?,?)'''
 stoptimes = open("stop_times.txt") 
 stoptimes.readline().split(',') #skip info line
-
-stopTimeInsertSQL = '''INSERT INTO StopTime (tripID, arrivalTime, departureTime, stopID, stopSequence, shapeDistTravelled, stopHeadSign, pickupType, dropOffType) VALUES (?,?,?,?,?,?,?,?,?)'''
 for row in stoptimes:
+    row.rstrip()
     splitline = row.split(',')
-    for x in splitline:
-        x.strip()
-        print(x)
-    
+    cursor.execute(stopTimeInsertSQL, splitline)
+database.commit() 
 
 stopInsertSQL = '''INSERT INTO Stop (stopId, stopName, stopLat, stopLon, wheelchair, stopCode) VALUES (?,?,?,?,?,?)'''
 stops = open("stops.txt")
+stops.readline()
+for row in stops:
+    row.rstrip()
+    splitline = row.split(',')
+    cursor.execute(stopInsertSQL, splitline)
+database.commit()
 
 routeInsertSQL = '''INSERT INTO Route (routeID, routeShortName, routeLongName, routeType, routeColour, routeTextColour) VALUES (?,?,?,?,?,?)'''
 routes = open("routes.txt")
+routes.readline()
+for row in routes:
+    row.rstrip()
+    splitline = row.split(',')
+    cursor.execute(routeInsertSQL,splitline)
+database.commit()
 
 tripsInsertSQL = '''INSERT INTO Trip (routeID, serviceID, tripID, tripHeadSign, shapeID, blockID, directionID) VALUES (?,?,?,?,?,?,?)'''
 trips = open("trips.txt")
+trips.readline()
+for row in trips:
+    row.rstrip()
+    splitline = row.split(',')
+    cursor.execute(tripsInsertSQL,splitline)
+database.commit()
 
 DateInsertSQL = '''INSERT INTO CalendarDate (serviceID, date, exceptionType) VALUES (?,?,?)'''
+dates = open("calendar_dates.txt")
+dates.readline()
+for row in dates:
+    row.rstrip()
+    splitline = row.split(',')
+    cursor.execute(DateInsertSQL,splitline)
+database.commit()
 
-shpaeInsertSQL = '''INSERT INTO Shape (shapeID, shapePtLat, shapePtLon, tripHeadSign, shapePtSequence, shapeDistTravelled) VALUES (?,?,?,?,?,?)'''
+shapeInsertSQL = '''INSERT INTO Shape (shapeID, shapePtLat, shapePtLon, shapePtSequence, shapeDistTravelled) VALUES (?,?,?,?,?)'''
+shapes = open("shapes.txt")
+shapes.readline()
+for row in shapes:
+    row.rstrip()
+    splitline = row.split(',')
+    cursor.execute(shapeInsertSQL,splitline)
+database.commit()
